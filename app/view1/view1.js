328cc6be475ed0a957a9bf1060f4fc90647abcca
ads/kulandm <muthukumar.kulandaisamy@autodesk.com>
2015-01-29 14:10:35 +0800

    Test push 2

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -4,8 +4,8 @@ angular.module('myApp.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'PeopleCtrl'
+    //templateUrl: 'view1/view1.html',
+    //controller: 'PeopleCtrl'
   });
 }])
 
